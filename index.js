4c0a67b78b8759465951c3dc4ba7840d3241a47a
izerephoibe <phoibeizere438@gmail.com>
2026-07-29 04:04:36 -0700

    changes

--- index.js
+++ index.js
@@ -6,30 +6,92 @@
 //     else{
 //         document.write(i+"is odd number"+"</br>");
 //     }
-// }
+// } 
+// Write a JavaScript function named sayHello that prints “Hello, world!” when it is 
+// invoked. Call the function.’ 
+
 // function sayHello()
 // {
 //     document.write("Hello,world");
 // }
 // sayHello();
 // sayHello();
+
 // function greet(name){
 //     document.write(name+"<br>");
 // }
 // greet("mutoni");
 // greet("kalisa");
-function grade(marks){
-    if(marks>=80)
-    {
-        return "A"
-    }
-    if(marks>=60){
-        return "B"
-    }
-    if (marks>=50){
-        return "C"
-    }
-    return ("fail")
-}
-let marks=parseInt(prompt("enter marks"));
-document.write(grade(marks));
+
+// function grade(marks){
+//     if(marks>=80)
+//     {
+//         return "A"
+//     }
+//     if(marks>=60){
+//         return "B"
+//     }
+//     if (marks>=50){
+//         return "C"
+//     }
+//     return ("fail")
+// }
+// let marks=parseInt(prompt("enter marks"));
+// document.write(grade(marks));
+
+// function calculateSquare(a){
+//     return a**2
+// }
+//     document.write(calculateSquare(4));
+
+
+
+// function addnumber (a,b)
+// {
+//     return a+b;
+// }
+// console.log(addnumber(3,5));
+// function showMessage ()
+// {
+//     document.write("invoke it")
+// }
+// showMessage();
+
+// displaying date
+// const displayDate = function()
+// {
+//     document.write(Date());
+// }
+// displayDate()
+
+// const multiply =function (a,b)
+// {
+// return a*b;
+// }
+// document.write(multiply(3,2));
+// const person={
+//     name:"john",
+//     age:25,
+//     'favorite color':'blue'
+// };
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person['favorite color']);
+// const student={
+//     name:"kevin",
+//     age:30,
+//     marks:70
+
+// };{
+// console.log(student.name);
+// console.log(student.age);
+// console.log(student.marks);
+// }
+// const product={
+//     name:"banana",
+//     price:600,
+//     quantity:300
+// };
+// let totalCost = product.price* product.quantity;
+// console.log(product.name);
+// console.log("total cost:"+totalCost);
