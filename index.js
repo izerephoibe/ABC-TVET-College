51f8560e79017964a44d5bbe82ba37a9262670be
izerephoibe <phoibeizere438@gmail.com>
2026-08-02 21:43:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -192,5 +192,31 @@
 //   else{
 //     console.log("oddNumber")
 //   }
+// //exerice 4
+// let name=["Alice","john","Grace","kevine"];
+// let username=prompt("enter nama:");
+// if(username){
+//     console.log(username+"exists in the array");
 // }
+// else{
+//     console.log(username+"not exist in array");
+// }
+// exerice 5
+// let number=[10,20,30,40,50,];
+// let sum =0;
+// for(let i=0;i< number.length;i++){
+//     sum+number[i];
+// }
+// console.log("totalsum="+sum);
+
+// // exerice 6
+
+// let number=[10,20,30,40,50,];
+// for(let i=number.length; i>=0;i--){
+// console.log(number[i]);
+// }
+
+let scores=[40,30,50,78,89,55,];
+let count=0;
+for(let)
 
